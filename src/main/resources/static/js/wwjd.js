f1dbcebcda7f1f212b3b476b45887e5c7c02242d
Matt Germeyer <matto483@gmail.com>
2017-06-06 09:06:22 -0500

    js playback

--- src/main/resources/static/js/wwjd.js
+++ src/main/resources/static/js/wwjd.js
@@ -1,5 +1,14 @@
 $( document ).ready(function() {
-  $('#addJimiId').click(function(){
-    $('#formId').attr('action', "/jimi").submit();
-  });
+//  $('#addJimiId').click(function(){
+//    $('#formId').attr('action', "/jimi").submit();
+//  });
+    console.log('loading')
+    alert('page loaded')
+//  var audio = new Audio('http://localhost:8080/pat1645.wav');
+//  alert("playing wav")
+//  audio.play();
+
 });
+
+
+
