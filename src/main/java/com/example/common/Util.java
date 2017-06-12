dd1689e51d8b39ff08e584b7ce7dab8777442de8
Matt Germeyer <matto483@gmail.com>
2017-06-11 19:01:29 -0500

    util class

--- src/main/java/com/example/common/Util.java
+++ src/main/java/com/example/common/Util.java
@@ -1,10 +1,56 @@
 package com.example.common;
 
+import com.example.service.ChordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import java.util.List;
+
 /**
  * Created by MattyG on 6/9/17.
  */
+@Service
 public class Util {
 
+    @Autowired
+    ChordService chordService;
+
+    public List<String> chordsFromGenre(String genre, String key){
+        List<String> songChords;
+        switch (genre){
+            case "blues":
+                songChords = chordService.generateBlues(key);
+                break;
+            case "popRock":
+                songChords = chordService.generatePopRock(key);
+                break;
+            case "soulful":
+                songChords = chordService.generateSoulful(key);
+                break;
+            case "classicRock":
+                songChords = chordService.generateSimpleRock(key);
+                break;
+            default:
+                songChords = chordService.generateBlues(key);
+        }
+        return songChords;
+    }
+
+    public Model genreForModel(String genre, Model model){
+
+        if (genre.equals("classicRock")) {
+            model.addAttribute("genretext", "Simple Rock");
+
+        } else if (genre.equals("blues")) {
+            model.addAttribute("genretext", "blues");
+        } else if (genre.equals("popRock")) {
+            model.addAttribute("genretext", "Pop-Rock");
+        } else {
+            model.addAttribute("genretext", "Soulful tune");
+        }
+    return model;
+    }
 
 
 }
--- src/main/java/com/example/controller/JimiController.java
+++ src/main/java/com/example/controller/JimiController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import com.example.common.Util;
 import com.example.domain.Song;
 import com.example.service.ChordService;
 import com.example.service.KeyService;
@@ -26,6 +27,8 @@ public class JimiController {
     ChordService chordService;
     @Autowired
     SongService songService;
+    @Autowired
+    Util util;
 
     @GetMapping("/project")
     public String project() {
@@ -47,36 +50,12 @@ public class JimiController {
     }
 
     @GetMapping("/genre")
-    public String getGenre(@RequestParam(value = "key", required = true) String key,
-                                 @RequestParam(value = "genre", required = true) String genre,
-                                 Model model) {
-        List<String> songChords;
-        switch (genre){
-            case "blues":
-                songChords = chordService.generateBlues(key);
-                break;
-            case "popRock":
-                songChords = chordService.generatePopRock(key);
-                break;
-            case "soulful":
-                songChords = chordService.generateSoulful(key);
-                break;
-            case "classicRock":
-                songChords = chordService.generateSimpleRock(key);
-                break;
-            default:
-                songChords = chordService.generateBlues(key);
-        }
-        if (genre.equals("classicRock")){
-            model.addAttribute("genretext", "Simple Rock");
+    public String getGenre(
+            @RequestParam(value = "key", required = true) String key,@RequestParam(value = "genre", required = true) String genre,
+                           Model model) {
 
-        } else if (genre.equals("blues")){
-            model.addAttribute("genretext", "blues");
-        } else if (genre.equals("popRock")){
-            model.addAttribute("genretext","Pop-Rock");
-        } else {
-            model.addAttribute("genretext","Soulful tune");
-        }
+        List<String>  songChords = util.chordsFromGenre(genre, key);
+        util.genreForModel(genre, model);
 
         model.addAttribute("genre", genre);
         model.addAttribute("key", key);
@@ -90,35 +69,9 @@ public class JimiController {
                        @RequestParam(value = "genre", required = true) String genre,
                        Model model) {
 
-        System.out.println("delete this lagter");
+        List<String> songChords = util.chordsFromGenre(genre, key);
+        util.genreForModel(genre, model);
 
-        List<String> songChords;
-        switch (genre){
-            case "blues":
-                songChords = chordService.generateBlues(key);
-                break;
-            case "popRock":
-                songChords = chordService.generatePopRock(key);
-                break;
-            case "soulful":
-                songChords = chordService.generateSoulful(key);
-                break;
-            case "classicRock":
-                songChords = chordService.generateSimpleRock(key);
-                break;
-            default:
-                songChords = chordService.generateBlues(key);
-        }
-        if (genre.equals("classicRock")){
-            model.addAttribute("genretext", "Simple Rock");
-
-        } else if (genre.equals("blues")){
-            model.addAttribute("genretext", "blues");
-        } else if (genre.equals("popRock")){
-            model.addAttribute("genretext","Pop-Rock");
-        } else {
-            model.addAttribute("genretext","Soulful tune");
-        }
         model.addAttribute("genre", genre);
         model.addAttribute("key", key);
         model.addAttribute("songChords", songChords);
@@ -130,26 +83,19 @@ public class JimiController {
             @RequestParam(value = "key", required = true) String key,
             @RequestParam(value = "genre", required = true) String genre,
             @RequestParam(value = "title", required = true) String title,
-                               Model model) {
+            Model model) {
 
         //add the song given 3 parameters
         songService.add(title, key, genre);
+
         //get list of songs from songservice
         List<Song> songs = songService.findAllByUsername();
-
-
-        model.addAttribute("songs",songs);
-
-
+        model.addAttribute("songs", songs);
         return "songManagement";
     }
 
     @GetMapping("/manageSongs")
     public String seeAllSongsForUser(Model model) {
-        List<Song> songs = songService.findAllByUsername();
-
-
-
         model.addAttribute("songs", songService.findAllByUsername());
 
         return "songManagement";
@@ -161,7 +107,7 @@ public class JimiController {
             @RequestParam(value = "chord", required = true) String chord,
             @RequestParam(value = "songChords", required = false) List<String> songChords, Model model) {
 
-        if (songChords == null){
+        if (songChords == null) {
             songChords = new ArrayList<>();
         }
         songChords.add(chord);
@@ -178,21 +124,4 @@ public class JimiController {
         return "song";
     }
 
-//    @PostMapping("/jimi")
-//    public String addJimiChord(@RequestParam(value = "key", required = true) String key,
-//                               @RequestParam(value = "songChords", required = false) List<String> songChords,
-//                               Model model) {
-//        if (songChords == null){
-//            songChords = new ArrayList<>();
-//        }
-//        //Add the next chord via jimiPickNextChord Method.
-//        chordService.jimiPickNextChord(songChords);
-//        List<String> suggestedChords = keyService.getVandVIIChordsFromKey(key);
-//        suggestedChords.addAll(chordService.getBasicChords(key));
-//        Set<String> suggestedChordSet = new TreeSet<>(suggestedChords);
-//        model.addAttribute("chords", suggestedChordSet);
-//        model.addAttribute("key", key);
-//        model.addAttribute("songChords", songChords);
-//        return "song";
-//    }
 }
